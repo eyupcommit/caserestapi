619891d6c42b3617a11d1803c2147838c1de80aa
eyupcommit <eyup_halis487@hotmail.com>
2020-05-22 03:53:09 +0300

    validation add

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+//yuklemis oldugumuz paketleri tanımladık
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express()
+//ara katmanda json datanın parse işlemi sağlandı
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+//mongo db ile bağlantı kuruldu
 MongoClient.connect('mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-case-study', { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
     if (err) throw err;
+    //calisacağimiz database tanımlandı
     var dbo = db.db("getir-case-study");
+    //api için fonksiyonumuz oluşturduk
     function postdata(req,res) {
+        //requestin body sini aldik
         let body = req.body
+        //requestteki tarih kontrolu yapildi
+        if(isNaN(Date.parse("\""+new Date(body.startDate)+"\""))||isNaN(Date.parse("\""+new Date(body.endDate)+"\"")))
+        {
+            res.json({
+                "code": 2,
+                "msg": "Error startDate endDate  tarih olmali",
+                "records": []
+            })
+        }
+        //requestteki sayi kontrolu yapildi
+        else if(typeof body.minCount!='number' || typeof  body.maxCount!='number'){
+            res.json({
+                "code": 3,
+                "msg": "Error minCount maxCount icin sayi olmali",
+                "records": []
+            })
+        }  
+        //requestteki degerlerin buyuk kucuk kontrolu yapildi
+        else if (body.startDate>body.endDate || body.minCount>body.maxCount){
+            res.json({
+                "code": 4,
+                "msg": "Error startDate endDate den buyuk veya minCount maxCount degerinden buyuk olamaz",
+                "records": []
+            })
+        }   
+        else{
+            //createdAt totalCount değerlerimiz için buyuk kucuk aralik query sini olusturduk
         let query = {
             createdAt: {
                 $gte: new Date(body.startDate),
@@ -18,22 +51,27 @@ MongoClient.connect('mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-
                 $lte: body.maxCount
             }
         }
+        //collection dan istenilen response a gore aggregate func olusturduk
         dbo.collection("records").aggregate([
             {
+                //totalcountumuz icin yeni bir alan ekleyip countslarin toplami alındı
                  "$addFields": { 
                      "totalCount": { "$sum": "$counts"}
                      }
             },
             {
+                // yukarda olusturdugumuz querymizi collectiondaki eslesmeleri sagladik
                 "$match": query
             },
-            { $project : {
+            {//formatta uygun response icin alanlarimizi duzenledik
+                 $project : {
                 _id: 0,
                 createdAt : 1 ,
                 key : 1,
                 totalCount:1
              } 
             }
+            //sonucumuzu dondurduk
             ]).toArray().then((result)=>{
                 if (!result) {
                     res.json({
@@ -51,9 +89,10 @@ MongoClient.connect('mongodb://dbUser:dbPassword1@ds249623.mlab.com:49623/getir-
             })
             
         
-        
-    }    
+        } 
+    }      
     app.post('/postdata', postdata)
 
 });
+//heroku ile uyumlu olan 5000 portu secildi
 app.listen(process.env.PORT || 5000, () => console.log("server 5000 portunda calismaktadir"))
